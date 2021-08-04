b63cca30e4a191a64958389807503e04937fb9fa
TBoonX <tboonx@googlemail.com>
2021-08-04 10:29:14 +0200

    Save work

--- RDFUnit/prepare_ttl_files.sh
+++ RDFUnit/prepare_ttl_files.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-SPARQL_ENDPOINT="https://sparql.stream-dataspace.net/sparql"
-SPARQL_GRAPH_DATA="http://test.de/"
-SPARQL_GRAPH_SHAPES="http://test.de/shapes/"
 DIR_REPORT_BASE="/var/www/html/reports"
 ID=$1
 
@@ -13,10 +10,11 @@ curl -G $SPARQL_ENDPOINT  --data-urlencode query='construct {  ?s ?p ?o } where 
 # Execute RDFUnit
 java -jar /app/rdfunit-validate.jar -d ./data.ttl -s ./shapes.ttl -f /tmp/ -o turtle --result-level shacl -A >> /proc/1/fd/1
 
-# Prepare data file for Jekyll RDF
-sed -i -e 's/urn:uuid:/http:\/\/stream-dataspace.net\/reports\//g' /tmp/results/._data.ttl.shaclTestCaseResult.ttl
+# Prepare data file for Jekyll RDF: prepare URI and combine the new triples with the old ones
+sed -i -e "s/urn:.*/http:\/\/stream-dataspace.net\/reports\/$ID>/g" /tmp/results/._data.ttl.shaclTestCaseResult.ttl
 mkdir -p /var/www/html/reports/_data/
 cat /tmp/results/._data.ttl.shaclTestCaseResult.ttl >> /var/www/html/reports/_data/report.ttl
+# TODO: delete old triples of the same URI with RdfProcessingToolkit before adding new ones
 rapper -i turtle -o turtle /var/www/html/reports/_data/report.ttl > /var/www/html/reports/_data/report2.ttl
 rm -f /var/www/html/reports/_data/report.ttl
 mv /var/www/html/reports/_data/report2.ttl /var/www/html/reports/_data/report.ttl
